6a9bd730e7379eff5023161d009fc9c90251a7e9
AO Industries, Inc <support@aoindustries.com>
2020-09-08 03:35:24 -0500

    Tab indent

--- src/main/java/com/aoindustries/aoserv/webservices/v1_0/Credentials.java
+++ src/main/java/com/aoindustries/aoserv/webservices/v1_0/Credentials.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2013, 2017 by AO Industries, Inc.,
+ * Copyright 2009-2013, 2017, 2020 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
@@ -12,40 +12,40 @@ import com.aoindustries.aoserv.client.dto.UserName;
  */
 public class Credentials {
 
-    private String locale;
-    private UserName username;
-    private String password;
-    private UserName switchUser;
+	private String locale;
+	private UserName username;
+	private String password;
+	private UserName switchUser;
 
-    public String getLocale() {
-        return locale;
-    }
+	public String getLocale() {
+		return locale;
+	}
 
-    public void setLocale(String locale) {
-        this.locale = locale;
-    }
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
 
-    public UserName getUsername() {
-        return username;
-    }
+	public UserName getUsername() {
+		return username;
+	}
 
-    public void setUsername(UserName username) {
-        this.username = username;
-    }
+	public void setUsername(UserName username) {
+		this.username = username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public UserName getSwitchUser() {
-        return switchUser;
-    }
+	public UserName getSwitchUser() {
+		return switchUser;
+	}
 
-    public void setSwitchUser(UserName switchUser) {
-        this.switchUser = switchUser;
-    }
+	public void setSwitchUser(UserName switchUser) {
+		this.switchUser = switchUser;
+	}
 }
